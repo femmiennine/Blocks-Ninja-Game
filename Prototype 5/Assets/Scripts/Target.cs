eb91fdb01e0977d613a6e8068655d1b96fb48ec5
Frances Fogarty <53113092+femmiennine@users.noreply.github.com>
2023-01-31 13:42:44 +0100

    added game manager

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabs the component Rigidbody
+        targetRb = GetComponent<Rigidbody>();
+        // Adds upward force randomly, immediately
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        // Adds rotation(torque) force randomly, immediately
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        // Grabs the position of the crate and create new random position in between x and y. Z not included
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
